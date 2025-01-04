a48a176373e57a0381e28e8dd93313a0169b3f4a
Bastiaan Stroosnijder <bas@strootje.com>
2025-01-04 20:16:39 +0100

    trailing slash

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,7 +9,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
-          <base href="/site-umarova" />
+          <base href="/site-umarova/" />
           {assets}
         </head>
         <body>
